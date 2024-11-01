d7099eead380407384ffb20a2e4898e15c2a23fe
David Turton <dturton@etrivo.com>
2024-11-01 10:59:01 -0400

    Update trackShopifyFieldUpdates_UE.js

--- trackShopifyFieldUpdates_UE.js
+++ trackShopifyFieldUpdates_UE.js
@@ -1,9 +1,32 @@
 /**
  * @NApiVersion 2.x
  * @NScriptType UserEventScript
+ * 
+ * Description:
+ * This script updates a custom date field, 'custrecord_last_product_info_updated', on an item record whenever specified fields are changed. It is triggered during the 'afterSubmit' event of record creation or editing.
+ * 
+ * Purpose:
+ * To maintain an accurate timestamp of when critical product information was last updated, facilitating efficient data synchronization with external systems.
+ *
+ * Modules Required:
+ * - N/record: Used to interact with NetSuite records and perform updates.
+ *
+ * Trigger:
+ * - After Submit: Executes after a record is created or edited.
+ *
+ * Fields Monitored:
+ * - field1
+ * - field2
+ * - field3
+ *
+ * Logic:
+ * 1. The script checks if the current operation is a record creation or edit.
+ * 2. It compares the new and old values of specified fields.
+ * 3. If any monitored field has changed, it updates the 'custrecord_last_product_info_updated' field with the current date and time.
  */
 define(['N/record'], function(record) {
     function afterSubmit(context) {
+        // Exit if not a create or edit operation
         if (context.type !== context.UserEventType.CREATE && context.type !== context.UserEventType.EDIT) {
             return;
         }
@@ -11,13 +34,15 @@ define(['N/record'], function(record) {
         var newRecord = context.newRecord;
         var oldRecord = context.oldRecord;
         
-        // List of fields to monitor
+        // List of fields to monitor for changes
         var fieldsToMonitor = ['field1', 'field2', 'field3'];
         
+        // Check if any monitored field has changed
         var shouldUpdateDate = fieldsToMonitor.some(function(fieldId) {
             return newRecord.getValue(fieldId) !== oldRecord.getValue(fieldId);
         });
         
+        // Update the custom date field if changes are detected
         if (shouldUpdateDate) {
             record.submitFields({
                 type: newRecord.type,
